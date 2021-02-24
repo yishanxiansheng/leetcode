130e762ce677fd368fa9bde7ba4274248850389a
yishanxiansheng <290685662@qq.com>
2021-02-24 17:47:07 +0800

    aa

---
+++ leetcode/app/src/test/java/com/example/testmaven/thread/TestCASAndLock.java
@@ -0,0 +1,22 @@
+package com.example.testmaven.thread;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author heshufan
+ * @date 2021/2/24
+ */
+
+public class TestCASAndLock {
+    public int num = 0;
+    public AtomicInteger integer = new AtomicInteger(0);
+    public AtomicInteger lock = new AtomicInteger(0);
+
+    public void add(){
+        
+    }
+
+    public static void main(String[] agrs){
+
+    }
+}
